d628e9f9a793e21ce7d40639ee281fbad3b7d544
cristiantoong <cristiantoong@gmail.com>
2021-02-17 07:31:55 +0800

    added modal

--- app/js/projects.js
+++ app/js/projects.js
@@ -4,13 +4,13 @@ const projects = [
         id: 1, 
         img: "./images/beautiful escape stylescape.jpg",
         alt: "beautiful escape resort",
-        category: "backend",
+        category: "frontend",
     },
     {
         id: 2, 
         img: "./images/chatisfy mockup.png",
         alt: "chatisfy",
-        category: "design",
+        category: "designs",
     },
     {
         id: 3, 
@@ -30,11 +30,43 @@ const projects = [
         alt: "cat steel corp",
         category: "frontend",
     },
+    {
+        id: 6, 
+        img: "./images/shevi cafe desktop.png",
+        alt: "shevi cafe",
+        category: "frontend",
+    },
 
+]
+
+const projectsModal = [
+    {
+        id: 1,
+        img: "./images/beautiful escape stylescape.jpg",
+        title: "Beautiful Escape Private Resort",
+        title1: "TECHNOLOGIES",
+        technologies: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma"],
+        github: "https://github.com/cristiantoong/beautifulescaperesort",
+        website:"https://cristiantoong.github.io/beautifulescaperesort/",
+        imgMockup: "images/Beautiful Escape Resort Device Mockup.png",
+        
+    },
+    {
+        id: 1,
+        img: "./images/chatisfy mockup.png",
+        title: "Chatisfy",
+        title1: "TECHNOLOGIES",
+        technologies: ["Webflow", "Figma"],
+        github: "https://github.com/cristiantoong/beautifulescaperesort",
+        website:"https://cristiantoong.github.io/beautifulescaperesort/",
+        imgMockup: "images/Beautiful Escape Resort Device Mockup.png",
+        
+    },
 ]
 
 const cards = document.getElementById('cards');
 const btnContainer = document.querySelector("#filter-btns");
+const filterBtns = document.querySelectorAll('#filter-btn');
 // console.log(filterBtns);
 
 // load items
@@ -42,14 +74,16 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //displayProjects(projects);
 
-    displayProjects2(projects);
+    //displayProjects2(projects);
+
+    modal();
 
     // const categories = projects.map(function(project){
     //     return project.category
     // });
     // console.log(categories);
 
-    displayMenuButtons();
+    //displayMenuButtons();
     
 
 });
@@ -70,9 +104,9 @@ function displayProjects(menuItems){
 
 //another way to display objects in html
 function displayProjects2(menuItems){
-    let displayProject = menuItems.map(function(project){
+    let displayProject = menuItems.map(function(project, index){
         return `
-        <div class="projects__card" id="card">
+        <div class="projects__card p${index+1}" id="card">
         <img src="${project.img}" alt="${project.alt}">
         </div> 
     `;
@@ -81,7 +115,7 @@ function displayProjects2(menuItems){
     displayProject = displayProject.join("");
     cards.innerHTML = displayProject;
 }
-
+filterItems();
 
 
 function displayMenuButtons(){
@@ -105,8 +139,14 @@ function displayMenuButtons(){
     }).join("");
     console.log(categoryBtns);
     btnContainer.innerHTML = categoryBtns;
-    const filterBtns = document.querySelectorAll('#filter-btn');
+    
+    filterItems();
+    
+}
 
+
+function filterItems(){
+    const filterBtns = document.querySelectorAll('#filter-btn');
     // Filter Items
     filterBtns.forEach(function(btn){
         btn.addEventListener('click', function(e){
@@ -133,6 +173,92 @@ function displayMenuButtons(){
             } else {
                 displayProjects2(projectCategory);
             }
+
+            //Display Modal When click
+            modal();
+            
         });
+
+        
     });
+
+    
 }
+
+
+// MODAL
+function modal(){
+    const modalExitBtn = document.getElementById('modal-exit-btn');
+    const modal = document.getElementById('modal');
+    const modals = document.querySelectorAll('.modal');
+    const cards1 = document.querySelectorAll('#card');
+    const prevBtn = document.getElementById('prev-btn');
+    const nextBtn = document.getElementById('next-btn');
+    let modalContent = document.querySelector('.modal-content');
+    let modalList = [];
+    let counter = 0;
+    console.log(cards1)
+
+    // Exit button
+    modalExitBtn.addEventListener('click', function(){
+        modal.style.display = 'none';
+    });
+
+
+    // Hide modal on outside click
+    window.addEventListener('click', function(e){
+        if (e.target == modal){
+            modal.style.display = 'none'
+        } else {
+            return false
+        }
+        modal.classList.toggle('animateShow');
+
+        
+    });
+
+
+
+    cards1.forEach(function(project, index){
+        project.addEventListener('click', function(e){
+            //show modal
+            modal.style.display = 'block';
+            modal.style.transition = 'all 0.5s ease-in;';
+            modalContent.style.transition = 'all 0.5s ease-in;';
+            modal.classList.toggle('animateShow');
+            // console.log(e.target.parentElement, index)
+
+            
+            
+            // display each project image content when click
+            if(e.target.parentElement.classList.contains(`p${index+1}`)){
+                modalContent.innerHTML = modals[index].innerHTML;
+            }
+
+        });  
+    });
+
+
+
+    // next and prev buttons for Modal slider
+    prevBtn.addEventListener('click', function(){
+        counter--;
+
+        if(counter < 0){
+            counter = modals.length - 1;
+        }
+
+        modalContent.innerHTML = modals[counter].innerHTML;
+
+    })
+
+    nextBtn.addEventListener('click', function(){
+        counter++;
+        if (counter > modals.length - 1) {
+        counter = 0;
+        }
+        modalContent.innerHTML = modals[counter].innerHTML;
+    })
+}
+
+modal();
